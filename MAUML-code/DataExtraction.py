a3c15a552102b9de111b397352a4b6ad0fdfc560
Luke Lunn <lul12@aber.ac.uk>
2019-04-01 16:54:59 +0100

    seperated data in sets of 50 frames

--- MAUML-code/DataExtraction.py
+++ MAUML-code/DataExtraction.py
@@ -1,28 +1,31 @@
 import pandas as pd
 import statistics as stat
+import numpy as np
 
 
 def parse_data():
+    file_name = ""
     df = pd.DataFrame
     incorrect_file = True
     while incorrect_file:
         try:
             file_name = input("Please enter the file you would like to load: ")
-            raw_data_set_full = pd.read_csv("csv_files/" + file_name,
-                                            header=4,
-                                            skip_blank_lines=True)
+            if file_name.__contains__(".csv"):
+                raw_data_set_full = pd.read_csv("csv_files/" + file_name,
+                                                header=4,
+                                                skip_blank_lines=True)
 
-            raw_data_set_full.drop(['Unnamed: 0', 'Unnamed: 1'], axis=1, inplace=True)
+                raw_data_set_full.drop(['Unnamed: 0', 'Unnamed: 1'], axis=1, inplace=True)
 
-            raw_data_set_full.dropna(how='all', inplace=True)
+                raw_data_set_full.dropna(how='all', inplace=True)
 
-            df = raw_data_set_full.values
+                df = raw_data_set_full.values
+
+                incorrect_file = False
 
         except FileNotFoundError:
             incorrect_file = True
             print("Incorrect file name, please try again...")
-        else:
-            incorrect_file = False
 
     return df
 
@@ -30,9 +33,10 @@ def parse_data():
 def feature_extract(this_list):
     minim = min(this_list)
     maxim = max(this_list)
+    range = minim - maxim
     average = stat.mean(this_list)
     stdevi = stat.stdev(this_list)
-    feature_set = [minim, maxim, average, stdevi]
+    feature_set = [minim, maxim, range, average, stdevi]
     return feature_set
 
 
@@ -61,8 +65,52 @@ data_separated = [r_sho, arm_si, r_elb, r_wri,
                   r_hip, leg_si, r_kne, r_ank,
                   l_hip, l_kne, l_ank]
 
-# for i in range(len(data_separated)):
-#     print("\n\n")
-#     print(data_separated[i][0])
+frame_set_len = r_sho.shape[0] // 50
 
-print(data)
+right_shoulder_f_sets = []
+arm_side_identifier_f_sets = []
+right_elbow_f_sets = []
+right_wrist_f_sets = []
+chest_f_sets = []
+small_of_back_f_sets = []
+left_shoulder_f_sets = []
+left_elbow_f_sets = []
+left_wrist_f_sets = []
+right_hip_f_sets = []
+leg_side_identifier_f_sets = []
+right_knee_f_sets = []
+right_ankle_f_sets = []
+left_hip_f_sets = []
+left_knee_f_sets = []
+left_ankle_f_sets = []
+
+frame_rate = 50
+
+for i in range(frame_set_len):
+    right_shoulder_f_sets.append(r_sho[i:i + frame_rate, :])
+    arm_side_identifier_f_sets.append(r_sho[i:i + frame_rate, :])
+    right_elbow_f_sets.append(r_sho[i:i + frame_rate, :])
+    right_wrist_f_sets.append(r_sho[i:i + frame_rate, :])
+    chest_f_sets.append(r_sho[i:i + frame_rate, :])
+    small_of_back_f_sets.append(r_sho[i:i + frame_rate, :])
+    left_shoulder_f_sets.append(r_sho[i:i + frame_rate, :])
+    left_elbow_f_sets.append(r_sho[i:i + frame_rate, :])
+    left_wrist_f_sets.append(r_sho[i:i + frame_rate, :])
+    right_hip_f_sets.append(r_sho[i:i + frame_rate, :])
+    leg_side_identifier_f_sets.append(r_sho[i:i + frame_rate, :])
+    right_knee_f_sets.append(r_sho[i:i + frame_rate, :])
+    right_ankle_f_sets.append(r_sho[i:i + frame_rate, :])
+    left_hip_f_sets.append(r_sho[i:i + frame_rate, :])
+    left_knee_f_sets.append(r_sho[i:i + frame_rate, :])
+    left_ankle_f_sets.append(r_sho[i:i + frame_rate, :])
+    i += frame_rate
+
+data_for_feature_extraction = np.array([right_shoulder_f_sets, arm_side_identifier_f_sets,
+                                        right_elbow_f_sets, right_wrist_f_sets,
+                                        left_shoulder_f_sets, left_elbow_f_sets, left_wrist_f_sets,
+                                        chest_f_sets, small_of_back_f_sets,
+                                        right_hip_f_sets, leg_side_identifier_f_sets, right_knee_f_sets,
+                                        right_ankle_f_sets,
+                                        left_hip_f_sets, left_knee_f_sets, left_ankle_f_sets])
+
+print(data_for_feature_extraction)
